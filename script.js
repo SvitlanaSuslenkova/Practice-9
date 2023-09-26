7199af91aee42687514a0660ebdf2ae1e48cc6d8
SvitlanaSuslenkova <136314004+SvitlanaSuslenkova@users.noreply.github.com>
2023-09-26 15:51:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-var btn = document.querySelectorAll(".rate");
-var span = document.getElementsByTagName("span")[0];
-var result;
-const section1 = document.querySelector(".section1");
-const section2 = document.querySelector(".section2"); 
+const buttons = document.querySelectorAll(".rate");
+const span = document.getElementsByTagName("span")[0];
+let result;
+let section1 = document.querySelector(".section1");
+let section2 = document.querySelector(".section2"); 
+let submit = document.getElementById("submitt");
+let arrowback = document.getElementById("arrowback");
 
-for (let i = 0; i < btn.length; i++) {
-btn[i].addEventListener("click", function(){
-btn[i].classList.toggle("selected");
-result = btn[i].innerHTML;
+buttons.forEach(button=> {
+button.addEventListener("click", function(){
+button.classList.toggle("selected");
+result = button.innerHTML;
 span.innerHTML = result;
-});}
-
-for (let i = 0; i < btn.length; i++) {      
-btn[i].addEventListener("blur", function(){
-btn[i].classList.remove("selected");
-});}
+});
+button.addEventListener("blur", function(){
+button.classList.remove("selected");});
+});
   
-var submit = document.getElementById("submitt");
 submit.addEventListener("click", function()  {
 if (result==null) {alert ("Please, choose the rating!");}
 else {section1.style.display="none";
 section2.style.display="block";}
 });
 
-var bck = document.getElementById("bckar");
-bck.addEventListener("click", function()  {
+arrowback.addEventListener("click", function()  {
   section1.style.display="block";
   section2.style.display="none";
-});  
+}); 
